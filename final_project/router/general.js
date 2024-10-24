0cab780ef7e62f28ab0571f17199f756bfa0a7aa
phattharaphorn <64010659@kmitl.ac.th>
2024-10-25 04:50:01 +0700

    update final project

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,162 +3,116 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const axios = require('axios');
 
-const doesExist = (username)=>{
-  let userswithsamename = users.filter((user)=>{
-    return user.username === username
-  });
-  if(userswithsamename.length > 0){
-    return true;
-  } else {
-    return false;
-  }
+function fetchData(url) {
+    return new Promise((resolve, reject) => {
+        axios.get(url)
+            .then(response => resolve(response.data))
+            .catch(error => reject(error));
+    });
 }
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  const username = req.body.username;
-  const password = req.body.password;
-
-
-  if (username && password) {
-    if (!doesExist(username)) { 
-      users.push({"username":username,"password":password});
-      return res.status(200).json({message: "User successfully registred. Now you can login"});
-    } else {
-      return res.status(404).json({message: "User already exists!"});
-    }
-  } 
-  return res.status(404).json({message: "Unable to register user."});
-});
-
-public_users.get('/',function (req, res) {
-
-  res.send(JSON.stringify(books,null,4));
-});
-
-public_users.get('/isbn/:isbn',function (req, res) {
-
-  const ISBN = req.params.isbn;
+    const username = req.body.username;
+    const password = req.body.password;
   
-  res.send(books[ISBN])
- });
-
-public_users.get('/author/:author',function (req, res) {
-
-  let ans = []
-    for(const [key, values] of Object.entries(books)){
-        const book = Object.entries(values);
-        for(let i = 0; i < book.length ; i++){
-            if(book[i][0] == 'author' && book[i][1] == req.params.author){
-                ans.push(books[key]);
-            }
-        }
-    }
-    if(ans.length == 0){
-        return res.status(300).json({message: "Author not found"});
-    }
-    res.send(ans);
-});
-
-public_users.get('/title/:title',function (req, res) {
-
-  let ans = []
-  for(const [key, values] of Object.entries(books)){
-      const book = Object.entries(values);
-      for(let i = 0; i < book.length ; i++){
-          if(book[i][0] == 'title' && book[i][1] == req.params.title){
-              ans.push(books[key]);
-          }
+    if (username && password) {
+      if (!isValid(username)) { 
+        users.push({"username":username,"password":password});
+        return res.status(200).json({message: "User successfully registred. Now you can login"});
+      } else {
+        return res.status(404).json({message: "User already exists!"});    
       }
-  }
-  if(ans.length == 0){
-      return res.status(300).json({message: "Title not found"});
-  }
-  res.send(ans);
+    } 
+    return res.status(404).json({message: "Unable to register user."});
 });
 
-
-public_users.get('/review/:isbn',function (req, res) {
-  const ISBN = req.params.isbn;
-  res.send(books[ISBN].reviews)
-});
-
-function getBookList(){
-  return new Promise((resolve,reject)=>{
-    resolve(books);
-  })
-}
-
+// Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  getBookList().then(
-    (bk)=>res.send(JSON.stringify(bk, null, 4)),
-    (error) => res.send("denied")
-  );  
+  //Write your code here
+  return res.status(200).send(JSON.stringify(books,null,4));
 });
 
 
-function getFromISBN(isbn){
-  let book_ = books[isbn];  
-  return new Promise((resolve,reject)=>{
-    if (book_) {
-      resolve(book_);
-    }else{
-      reject("Unable to find book!");
-    }    
-  })
-}
 
+// Get the book list available in the shop
+public_users.get('/async/', function (req, res) {
+    fetchData('https://jan-7001.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/')
+        .then(fetchedBooks => res.status(200).json({ books: fetchedBooks }))
+        .catch(error => {
+            console.error('Error fetching books:', error.message);
+            return res.status(500).json({ error: 'Internal Server Error' });
+        });
+});
+
+// Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   const isbn = req.params.isbn;
-  getFromISBN(isbn).then(
-    (bk)=>res.send(JSON.stringify(bk, null, 4)),
-    (error) => res.send(error)
-  )
+  return res.status(200).send(books[isbn]);
  });
 
-
-function getFromAuthor(author){
-  let output = [];
-  return new Promise((resolve,reject)=>{
-    for (var isbn in books) {
-      let book_ = books[isbn];
-      if (book_.author === author){
-        output.push(book_);
-      }
-    }
-    resolve(output);  
-  })
-}
-
-public_users.get('/author/:author',function (req, res) {
-  const author = req.params.author;
-  getFromAuthor(author)
-  .then(
-    result =>res.send(JSON.stringify(result, null, 4))
-  );
+ public_users.get('/async/isbn/:isbn', function (req, res) {
+    fetchData(`https://jan-7001.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/isbn/${req.params.isbn}`)
+        .then(fetchedBooks => res.status(200).json({ books: fetchedBooks }))
+        .catch(error => {
+            console.error('Error fetching books:', error.message);
+            return res.status(500).json({ error: 'Internal Server Error' });
+        });
+});
+  
+// Get book details based on author
+public_users.get('/async/author/:author', function (req, res) {
+    fetchData(`https://jan-7001.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/author/${req.params.author}`)
+        .then(fetchedBooks => res.status(200).json(fetchedBooks))
+        .catch(error => {
+            console.error('Error fetching books:', error.message);
+            return res.status(500).json({ error: 'Internal Server Error' });
+        });
 });
 
-function getFromTitle(title){
-  let output = [];
-  return new Promise((resolve,reject)=>{
-    for (var isbn in books) {
-      let book_ = books[isbn];
-      if (book_.title === title){
-        output.push(book_);
-      }
-    }
-    resolve(output);  
-  })
-}
+public_users.get('/async/author/:author', async function (req, res) {
+    try {
+        const response = await axios.get('https://jan-7001.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/author/'+req.params.author);
+        
+        const fetchedBooks = response.data;
 
-public_users.get('/title/:title',function (req, res) {
-  const title = req.params.title;
-  getFromTitle(title)
-  .then(
-    result =>res.send(JSON.stringify(result, null, 4))
-  );
+        return res.status(200).json(fetchedBooks);
+    } catch (error) {
+        console.error('Error fetching books:', error.message);
+        return res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+
+
+// Get all books based on title
+public_users.get('/async/title/:title', function (req, res) {
+    fetchData(`https://jan-7001.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/title/${req.params.title}`)
+        .then(fetchedBooks => res.status(200).json(fetchedBooks))
+        .catch(error => {
+            console.error('Error fetching books:', error.message);
+            return res.status(500).json({ error: 'Internal Server Error' });
+        });
+});
+
+public_users.get('/async/title/:title', async function (req, res) {
+    try {
+        const response = await axios.get('https://jan-7001.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/title/'+req.params.title);
+        
+        const fetchedBooks = response.data;
+
+        return res.status(200).json(fetchedBooks);
+    } catch (error) {
+        console.error('Error fetching books:', error.message);
+        return res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+
+//  Get book review
+public_users.get('/review/:isbn',function (req, res) {
+    const isbn = req.params.isbn;
+    return res.send(books[isbn]["reviews"]);
 });
 
 module.exports.general = public_users;
